e1ee26546d4324e9ce66482c783119b90dd0ce3f
Muhammad Najmuddin Farid <farid.najmuddins@gmail.com>
2023-01-08 17:54:31 +0300

    runtime exec and report

--- SystemPROJE/src/system/main.java
+++ SystemPROJE/src/system/main.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
-
-
+import java.io.InputStream;
 
 import system.Process.Status;
 //
@@ -21,6 +19,12 @@ import system.Process.Status;
 public class main {
 	
 	public static void main(String[] args) throws IOException {
+		
+        // Run a java app in a separate system process
+        java.lang.Process proc = Runtime.getRuntime().exec("java -jar proje.jar");
+        // Then retreive the process output
+        InputStream in = proc.getInputStream();
+        InputStream err = proc.getErrorStream();
 	
 		if(args.length<=0)
 		{}
